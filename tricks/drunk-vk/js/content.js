a0cc5be822d9e38b451d936bffc7cb6e717bf422
GregTheMadMonk <yagreg7@gmail.com>
2021-11-23 20:57:56 +0300

    Fire drunk VK only once

--- tricks/drunk-vk/js/content.js
+++ tricks/drunk-vk/js/content.js
@@ -1,7 +1,10 @@
-function spin() {
-	Array.from(document.querySelectorAll(".image_status__statusImage, .emoji, .TopNavBtn__icon, .ui_actions_menu_icons, .left_count_wrap, .im_grid, .TopNavBtn__notifyCount")).forEach(
-		node => node.setAttribute("style", "transition: .7s; transform: rotate(" + (Math.random() * 360) + "deg)")
-	);
-	setTimeout(spin, 1000);
+if (!drunkvk_loaded) {
+	function spin() {
+		Array.from(document.querySelectorAll(".image_status__statusImage, .emoji, .TopNavBtn__icon, .ui_actions_menu_icons, .left_count_wrap, .im_grid, .TopNavBtn__notifyCount")).forEach(
+			node => node.setAttribute("style", "transition: .7s; transform: rotate(" + (Math.random() * 360) + "deg)")
+		);
+		setTimeout(spin, 1000);
+	}
+	spin();
 }
-spin();
+let drunkvk_loaded = true;
