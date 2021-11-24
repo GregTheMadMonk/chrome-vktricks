1cc7bea6101889fce50f7b9d10412578a4d01c5a
GregTheMadMonk <yagreg7@gmail.com>
2021-11-24 10:55:55 +0300

    Fix the previous fix

--- tricks/drunk-vk/js/content.js
+++ tricks/drunk-vk/js/content.js
@@ -1,4 +1,4 @@
-if (!drunkvk_loaded) {
+if (!document.body.hasAttribute("drunk")) {
 	function spin() {
 		Array.from(document.querySelectorAll(".image_status__statusImage, .emoji, .TopNavBtn__icon, .ui_actions_menu_icons, .left_count_wrap, .im_grid, .TopNavBtn__notifyCount")).forEach(
 			node => node.setAttribute("style", "transition: .7s; transform: rotate(" + (Math.random() * 360) + "deg)")
@@ -7,4 +7,4 @@ if (!drunkvk_loaded) {
 	}
 	spin();
 }
-let drunkvk_loaded = true;
+document.body.setAttribute("drunk", "true");
